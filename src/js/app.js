9b234ce4d0b5ae04b0611ba85174b4997b2b3d97
wcelmer <w.celmer@me.com>
2018-06-26 13:47:29 +0200

    flag icon, adress added

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,14 @@
 
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
+
+function setBackGround() {
+  let el = $('.currency li');
+  for (var i = 0; i < el.length; i++) {
+    let clas = el[i].className;
+
+    $(el[i]).css('background-image', 'url(' + './flag-icon/'+ clas + '.png' +')');
+  }
+}
+setBackGround();
