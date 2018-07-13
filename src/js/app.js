398b9683a11d247cd260b792c2572c3770063774
wcelmer <w.celmer@me.com>
2018-07-13 13:22:02 +0200

    delete timer

--- src/js/app.js
+++ src/js/app.js
@@ -11,17 +11,13 @@ $(function () {
   let ww = $( window ).width();
   let wh = $( window ).height();
   let traX, traY;
-  let timerSet;
   $(document).mousemove(function(e){
-    clearTimeout(timerSet);
-    timerSet = setTimeout(function () {
-      mouseX = e.pageX;
-      mouseY = e.pageY;
-      traX = ((4 * mouseX) / 570) + 40;
-      traY = ((4 * mouseY) / 570) + 50;
-      $(".title > h1").css({"background-position": traX + "%" + traY + "%"});
-      $(".title > h2").css({"background-position": traX + "%" + traY + "%"});
-    }, 10);
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+    traX = ((4 * mouseX) / 570) + 40;
+    traY = ((4 * mouseY) / 570) + 50;
+    $(".title > h1").css({"background-position": traX + "%" + traY + "%"});
+    $(".title > h2").css({"background-position": traX + "%" + traY + "%"});
   });
 
 // Flag icons adding in background
